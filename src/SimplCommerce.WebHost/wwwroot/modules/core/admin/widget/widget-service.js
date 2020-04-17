3f1070d2ad901c452561a4a96e2e46912a33daf9
Kiệt Phạm Quang Anh <phamquanganhkiet@gmail.com>
2020-04-17 21:24:54 +1000

    commit front side files

---
+++ src/SimplCommerce.WebHost/wwwroot/modules/core/admin/widget/widget-service.js
@@ -0,0 +1,28 @@
+﻿/*global angular*/
+(function () {
+    angular
+        .module('simplAdmin.core')
+        .factory('widgetService', widgetService);
+
+    /* @ngInject */
+    function widgetService($http) {
+        var service = {
+            getWidgets: getWidgets,
+            getWidgetInstances: getWidgetInstances,
+            deleteWidgetInstance: deleteWidgetInstance
+        };
+        return service;
+
+        function getWidgets() {
+            return $http.get('api/widgets');
+        }
+
+        function getWidgetInstances() {
+            return $http.get('api/widget-instances');
+        }
+
+        function deleteWidgetInstance(id) {
+            return $http.delete('api/widget-instances/' + id);
+        }
+    }
+})();
